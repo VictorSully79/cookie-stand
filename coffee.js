af915259e245432b4fae5a8c00dfcf4b7e97812a
VictorSully79 <victorsullivan@hotmail.com>
2021-01-19 21:44:21 -0800

    completed initial code for each store

--- coffee.js
+++ coffee.js
@@ -1,15 +1,148 @@
 'use strict';
 
-var cookieShop = {
-  location: 'Seattle',
-  customers: null,
-  hours:['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'],
-  sales: null,
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
-  generateSales: function (salesEstimate) {
+var seattle = {
+  location: "Seattle",
+  minCust:23,
+  maxCust:65,
+  avgCookie:6.3,
+  cookiesPerHour: [],
+  
+  generateSales: function() {
     
-    var seattleSales = Math.floor((Math.random() * 10) + 1);
-    console.log(seattleSales)
-    this.customers = seattleSales;
-  },
+    var randomSales = Math.floor((Math.random() * ((this.maxCust - this.minCust)+1) + this.minCust));
+    console.log('this is randomSales ' + randomSales);
+    var estimatedSales = Math.round(randomSales * this.avgCookie);
+    console.log('this is estimatedSales ' + estimatedSales);
+    
+    return estimatedSales;
+  }, 
 }
+
+for (var i =0; i < hours.length; i ++) {
+  var cookies = seattle.generateSales();
+  seattle.cookiesPerHour.push(cookies);
+}
+function generateCustomers(store) {
+  Math.floor(Math.random() * (store.maxCust - store.minCust + 1)) + store.minCust;
+}
+
+generateCustomers(store); 
+
+
+
+var tokyo = {
+  location: "Tokyo",
+  minCust:3,
+  maxCust:24,
+  avgCookie:1.2,
+  cookiesPerHour: [],
+  
+  generateSales: function() {
+    
+    var randomSales = Math.floor((Math.random() * ((this.maxCust - this.minCust)+1) + this.minCust));
+    console.log('this is randomSales ' + randomSales);
+    var estimatedSales = Math.round(randomSales * this.avgCookie);
+    console.log('this is estimatedSales ' + estimatedSales);
+    
+    return estimatedSales;
+  }, 
+}
+
+for (var i =0; i < hours.length; i ++) {
+  var cookies = tokyo.generateSales();
+  tokyo.cookiesPerHour.push(cookies);
+}
+function generateCustomers(store) {
+  Math.floor(Math.random() * (store.maxCust - store.minCust + 1)) + store.minCust;
+}
+
+generateCustomers(store); 
+
+
+var dubai = {
+  location: "Dubai",
+  minCust:3,
+  maxCust:24,
+  avgCookie:1.2,
+  cookiesPerHour: [],
+  
+  generateSales: function() {
+    
+    var randomSales = Math.floor((Math.random() * ((this.maxCust - this.minCust)+1) + this.minCust));
+    console.log('this is randomSales ' + randomSales);
+    var estimatedSales = Math.round(randomSales * this.avgCookie);
+    console.log('this is estimatedSales ' + estimatedSales);
+    
+    return estimatedSales;
+  }, 
+}
+
+for (var i =0; i < hours.length; i ++) {
+  var cookies = dubai.generateSales();
+  dubai.cookiesPerHour.push(cookies);
+}
+function generateCustomers(store) {
+  Math.floor(Math.random() * (store.maxCust - store.minCust + 1)) + store.minCust;
+}
+
+generateCustomers(store); 
+
+
+var paris = {
+  location: "Paris",
+  minCust:3,
+  maxCust:24,
+  avgCookie:1.2,
+  cookiesPerHour: [],
+  
+  generateSales: function() {
+    
+    var randomSales = Math.floor((Math.random() * ((this.maxCust - this.minCust)+1) + this.minCust));
+    console.log('this is randomSales ' + randomSales);
+    var estimatedSales = Math.round(randomSales * this.avgCookie);
+    console.log('this is estimatedSales ' + estimatedSales);
+    
+    return estimatedSales;
+  }, 
+}
+
+for (var i =0; i < hours.length; i ++) {
+  var cookies = paris.generateSales();
+  paris.cookiesPerHour.push(cookies);
+}
+function generateCustomers(store) {
+  Math.floor(Math.random() * (store.maxCust - store.minCust + 1)) + store.minCust;
+}
+
+generateCustomers(store); 
+
+
+var lima = {
+  location: "Laris",
+  minCust:3,
+  maxCust:24,
+  avgCookie:1.2,
+  cookiesPerHour: [],
+  
+  generateSales: function() {
+    
+    var randomSales = Math.floor((Math.random() * ((this.maxCust - this.minCust)+1) + this.minCust));
+    console.log('this is randomSales ' + randomSales);
+    var estimatedSales = Math.round(randomSales * this.avgCookie);
+    console.log('this is estimatedSales ' + estimatedSales);
+    
+    return estimatedSales;
+  }, 
+}
+
+for (var i =0; i < hours.length; i ++) {
+  var cookies = lima.generateSales();
+  lima.cookiesPerHour.push(cookies);
+}
+function generateCustomers(store) {
+  Math.floor(Math.random() * (store.maxCust - store.minCust + 1)) + store.minCust;
+}
+
+generateCustomers(store); 
